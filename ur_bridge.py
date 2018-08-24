3af537766042a37d9e8a68f663fbe38c84bcee42
YeTian <flymaxty@foxmail.com>
2018-08-24 12:47:13 +0800

    add ur bridge

--- ur_bridge.py
+++ ur_bridge.py
@@ -21,6 +21,7 @@ def trace(*args):
 
 def exit(signum, frame):
     print('UR bridge exit...')
+    arm.stop()
     arm.close()
     mqtt_client.disconnect()
 
@@ -29,16 +30,26 @@ def on_mqtt_connect(client, userdata, flags, rc):
     client.subscribe("iqr/cs-ras/topic/arm_request")
 
 def on_mqtt_message(client, userdata, msg):
+    tcp_pos = math3d.Transform()
     print(msg.topic+" "+str(msg.payload))
     if(msg.topic == "iqr/cs-ras/topic/arm_request"):
         data_json = json.loads(msg.payload)
-        if(data_json["type"] == "movel_tool"):
+        data_type = data_json["type"] 
+        if(data_type == "movel_tool"):
             data_list = data_json["matrix"]
+            data_list[3] = data_list[3] / 1000.0
+            data_list[7] = data_list[7] / 1000.0
+            data_list[11] = data_list[11] / 1000.0
             matrix = math3d.Transform(np.array(data_list).reshape(4,4))
+            # orient = math3d.Orientation()
+            # matrix.set_orient(orient)
+            accel = data_json["accel"]
+            velocity = data_json["velocity"]
+            print("data", data_list[3])
             print("movel_tool, before\n", arm.get_pose(), "after\n", matrix)
-            arm.movel_tool(matrix, acc=0.4, vel=0.4)
-        else:
-            trace("Unkown data")
+            arm.movel_tool(matrix, acc=accel, vel=velocity, wait=False)
+        elif(data_type == "stop"):
+            arm.stop()
     else:
         trace("Unkown topic")
 
@@ -56,6 +67,10 @@ if __name__ == '__main__':
 
     # loop forever
     # print("Start listening...")
-    mqtt_client.loop_forever()
+    # mqtt_client.loop_forever()
+
+    while(True):
+        trace("get pose", arm.get_pose())
+        mqtt_client.loop()
 
     print('Done')
